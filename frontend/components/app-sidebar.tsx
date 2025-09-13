e1011ce09b8ad263f3483ad9f540734348237205
keaneph <nearpharelle@gmail.com>
2025-09-13 13:13:40 +0800

    initial sidebar design

---
+++ frontend/components/app-sidebar.tsx
@@ -0,0 +1,76 @@
+import { TreePalm, Home, School, GraduationCap, User, Settings  } from "lucide-react"
+
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarHeader,
+  SidebarGroupLabel,
+  SidebarGroupContent,
+  SidebarMenuItem,
+  SidebarMenuButton,
+  SidebarMenu,
+} from "@/components/ui/sidebar"
+
+
+// Menu items.
+const items = [
+  {
+    title: "Dashboard",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Colleges",
+    url: "#",
+    icon: School,
+  },
+  {
+    title: "Programs",
+    url: "#",
+    icon: GraduationCap,
+  },
+  {
+    title: "Students",
+    url: "#",
+    icon: User,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
+
+export function AppSidebar() {
+  return (
+    <Sidebar collapsible="icon">
+      <SidebarHeader>
+        <SidebarMenuButton className="flex items-center gap-2 peer-data-[active=true]/menu-button:opacity-100">
+          <TreePalm className="h-5 w-5" />
+          <span>Maktab</span>
+        </SidebarMenuButton>
+      </SidebarHeader>
+      <SidebarContent>
+        <SidebarGroup>
+          <SidebarGroupLabel>Application</SidebarGroupLabel>
+          <SidebarGroupContent>
+            <SidebarMenu>
+              {items.map((item) => (
+                <SidebarMenuItem key={item.title}>
+                  <SidebarMenuButton asChild>
+                    <a href={item.url}>
+                      <item.icon />
+                      <span>{item.title}</span>
+                    </a>
+                  </SidebarMenuButton>
+                </SidebarMenuItem>
+              ))}
+            </SidebarMenu>
+          </SidebarGroupContent>
+        </SidebarGroup>
+      </SidebarContent>
+    </Sidebar>
+  )
+}
