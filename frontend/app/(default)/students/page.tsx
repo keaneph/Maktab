67c53f60c117c3adce30954e8f775f4c5a69da73
keaneph <nearpharelle@gmail.com>
2025-09-18 18:50:21 +0800

    fixed site header for students page

--- frontend/app/(default)/students/page.tsx
+++ frontend/app/(default)/students/page.tsx
@@ -1,12 +1,12 @@
 import { SiteHeader } from "@/components/site-header";
 
-export default function ProgramPage() {
+export default function StudentPage() {
   return (
     <>
-      <SiteHeader title="Programs" />
+      <SiteHeader title="Students" />
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
         <div className="px-4 lg:px-6">
-          {/* Programs data table goes here */}
+          {/* Students data table goes here */}
         </div>
       </div>
     </>
